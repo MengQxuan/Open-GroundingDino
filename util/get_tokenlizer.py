cfeeda76920d8a9a44d2ad9a3e9a7b74c18d01aa
MengQxuan <mengqx1205@gmail.com>
2026-02-09 21:41:59 +0800

    Transformer 层剪枝 enc: 6 → 4，dec: 6 → 3 epoch_time：19min

--- util/get_tokenlizer.py
+++ util/get_tokenlizer.py
@@ -1,44 +1,12 @@
-# from transformers import AutoTokenizer, BertModel, BertTokenizer, RobertaModel, RobertaTokenizerFast
-# import os
-
-# def get_tokenlizer(text_encoder_type):
-#     if not isinstance(text_encoder_type, str):
-#         # print("text_encoder_type is not a str")
-#         if hasattr(text_encoder_type, "text_encoder_type"):
-#             text_encoder_type = text_encoder_type.text_encoder_type
-#         elif text_encoder_type.get("text_encoder_type", False):
-#             text_encoder_type = text_encoder_type.get("text_encoder_type")
-#         elif os.path.isdir(text_encoder_type) and os.path.exists(text_encoder_type):
-#             pass
-#         else:
-#             raise ValueError(
-#                 "Unknown type of text_encoder_type: {}".format(type(text_encoder_type))
-#             )
-#     print("final text_encoder_type: {}".format(text_encoder_type))
-#     tokenizer = AutoTokenizer.from_pretrained(text_encoder_type)
-#     print("load tokenizer done.")
-#     return tokenizer
-
-
-# def get_pretrained_language_model(text_encoder_type):
-#     if text_encoder_type == "bert-base-uncased" or (os.path.isdir(text_encoder_type) and os.path.exists(text_encoder_type)):
-#         return BertModel.from_pretrained(text_encoder_type)
-#     if text_encoder_type == "roberta-base":
-#         return RobertaModel.from_pretrained(text_encoder_type)
-
-#     raise ValueError("Unknown text_encoder_type {}".format(text_encoder_type))
-
-
-# 用 MiniLM替换BERT
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, BertModel, BertTokenizer, RobertaModel, RobertaTokenizerFast
 import os
-
 
 def get_tokenlizer(text_encoder_type):
     if not isinstance(text_encoder_type, str):
+        # print("text_encoder_type is not a str")
         if hasattr(text_encoder_type, "text_encoder_type"):
             text_encoder_type = text_encoder_type.text_encoder_type
-        elif isinstance(text_encoder_type, dict) and text_encoder_type.get("text_encoder_type", False):
+        elif text_encoder_type.get("text_encoder_type", False):
             text_encoder_type = text_encoder_type.get("text_encoder_type")
         elif os.path.isdir(text_encoder_type) and os.path.exists(text_encoder_type):
             pass
@@ -46,22 +14,54 @@ def get_tokenlizer(text_encoder_type):
             raise ValueError(
                 "Unknown type of text_encoder_type: {}".format(type(text_encoder_type))
             )
-
-    print("final text_encoder_type:", text_encoder_type)
-
-    tokenizer = AutoTokenizer.from_pretrained(text_encoder_type, use_fast=True)
+    print("final text_encoder_type: {}".format(text_encoder_type))
+    tokenizer = AutoTokenizer.from_pretrained(text_encoder_type)
     print("load tokenizer done.")
-
     return tokenizer
 
 
 def get_pretrained_language_model(text_encoder_type):
+    if text_encoder_type == "bert-base-uncased" or (os.path.isdir(text_encoder_type) and os.path.exists(text_encoder_type)):
+        return BertModel.from_pretrained(text_encoder_type)
+    if text_encoder_type == "roberta-base":
+        return RobertaModel.from_pretrained(text_encoder_type)
 
-    print("load text encoder:", text_encoder_type)
+    raise ValueError("Unknown text_encoder_type {}".format(text_encoder_type))
 
-    # allow local dir
-    if os.path.isdir(text_encoder_type) and os.path.exists(text_encoder_type):
-        return AutoModel.from_pretrained(text_encoder_type)
 
-    # allow any HF model (BERT / MiniLM / Distil / Roberta / etc.)
-    return AutoModel.from_pretrained(text_encoder_type)
+# # 用 MiniLM替换BERT
+# from transformers import AutoTokenizer, AutoModel
+# import os
+
+
+# def get_tokenlizer(text_encoder_type):
+#     if not isinstance(text_encoder_type, str):
+#         if hasattr(text_encoder_type, "text_encoder_type"):
+#             text_encoder_type = text_encoder_type.text_encoder_type
+#         elif isinstance(text_encoder_type, dict) and text_encoder_type.get("text_encoder_type", False):
+#             text_encoder_type = text_encoder_type.get("text_encoder_type")
+#         elif os.path.isdir(text_encoder_type) and os.path.exists(text_encoder_type):
+#             pass
+#         else:
+#             raise ValueError(
+#                 "Unknown type of text_encoder_type: {}".format(type(text_encoder_type))
+#             )
+
+#     print("final text_encoder_type:", text_encoder_type)
+
+#     tokenizer = AutoTokenizer.from_pretrained(text_encoder_type, use_fast=True)
+#     print("load tokenizer done.")
+
+#     return tokenizer
+
+
+# def get_pretrained_language_model(text_encoder_type):
+
+#     print("load text encoder:", text_encoder_type)
+
+#     # allow local dir
+#     if os.path.isdir(text_encoder_type) and os.path.exists(text_encoder_type):
+#         return AutoModel.from_pretrained(text_encoder_type)
+
+#     # allow any HF model (BERT / MiniLM / Distil / Roberta / etc.)
+#     return AutoModel.from_pretrained(text_encoder_type)
